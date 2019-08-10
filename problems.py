1c72b068bdd90c33616c39f93efa91de0680ab30
Nguyen Tran Nhan <pocolomos@gmail.com>
2019-08-10 15:18:24 +0700

    update problem to text2text problem

--- problems.py
+++ problems.py
@@ -43,7 +43,7 @@ class TranslateVivi(text_problems.Text2TextProblem):
     #aeiouyd = ['a', 'e', 'i', 'o', 'u', 'y', 'd']
     legal = ' !"#$%&\'()*+,-./0123456789:;<=>?@[\\]^_`abcdefghijklmnopqrstuvwxyzáàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵđ{|}~'
     punct = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    train_data = "%s/train.txt" % data_dir
+    train_data = "%s/train_data.txt" % data_dir
     with open(train_data, 'r') as f:
       lines = f.readlines()
       for line in lines:
--- run.sh
+++ run.sh
@@ -12,14 +12,71 @@ decode_hparams="beam_size=2,alpha=0.6"
 #--envi_ckpt=$envi_path 
 #--backtranslate_interactively
 
-#gen data
-python3 ./t2t_datagen.py \
---data_dir=./data/translate_vivi \
---tmp_dir=/tmp/translate_vivi \
---problem=translate_vivi
+while [ "$1" != "" ]; do
+    case $1 in
+        -g | --gen )           shift
+                                echo "Start to run t2t_gendata"
+                                python3 ./t2t_datagen.py \
+                                --data_dir=./data/translate_vivi \
+                                --tmp_dir=/tmp/translate_vivi \
+                                --problem=translate_vivi
+                                $@
+                                ;;
+        -t | --train )    echo "Start tp rim t2t_train"
+                            python3 ./t2t_trainer.py \
+                                --model='transformer' \
+                                --hparams_set=$hparams_set \
+                                --hparams='learning_rate_cosine_cycle_steps=50000' \
+                                --train_steps=1000 \
+                                --eval_steps=10 \
+                                --problem=translate_vivi \
+                                --data_dir=./data/translate_vivi \
+                                --output_dir=./output/translate_vivi
+                                --use_tpu=False \
+                                --worker_gpus=2 \
+                                $@
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
 
-#python3 ./t2t_trainer.py --model='transformer' --hparams_set=$hparams_set \
-#--hparams='learning_rate_cosine_cycle_steps=50000' \
-#--train_steps=1000 --eval_steps=10 \
-#--problem=translate_vivi --data_dir=./data/translate_vivi \
-#--output_dir=./output/translate_vivi --use_tpu=false
+
+if [ "$1" == "gen_data" ]; then
+    echo "Start to run t2t_gendata"
+    python3 ./t2t_datagen.py \
+	--data_dir=./data/translate_vivi \
+	--tmp_dir=/tmp/translate_vivi \
+	--problem=translate_vivi
+	$@
+else if [ "$1" == "train" ]; then
+    echo "Start tp rim t2t_train"
+    python3 ./t2t_trainer.py \
+        --model='transformer' \
+        --hparams_set=$hparams_set \
+        --hparams='learning_rate_cosine_cycle_steps=50000' \
+        --train_steps=1000 \
+        --eval_steps=10 \
+        --problem=translate_vivi \
+        --data_dir=./data/translate_vivi \
+        --output_dir=./output/translate_vivi
+        --use_tpu=False \
+        --worker_gpus=2 \
+        $@
+el
+    #python3 ./back_translate.py 
+    #--lang=vi \
+    #--decode_hparams=$decode_hparams \
+    #--model=$model_name \
+    #--hparams_set=$hparams_set \
+    #--vien_ckpt=$vien_path \
+    #--envi_ckpt=$envi_path 
+    #--backtranslate_interactively	
+    #$@
+fi
+
+
