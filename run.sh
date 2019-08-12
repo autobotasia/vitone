e5f8313a8951b59b4c70600b3f6b60f8ba092370
Nguyen Tran Nhan <pocolomos@gmail.com>
2019-08-12 09:55:31 +0700

    update decodig

--- run.sh
+++ run.sh
@@ -2,27 +2,22 @@
 model_name=transformer
 hparams_set=transformer_tiny
 vivi_path=./checkpoints/translate_vivi_tiny/avg
-decode_hparams="beam_size=2,alpha=0.6"
+decode_hparams="beam_size=4,alpha=0.6"
 
-#python3 ./back_translate.py --lang=vi \
-#--decode_hparams=$decode_hparams \
-#--model=$model_name \
-#--hparams_set=$hparams_set \
-#--vien_ckpt=$vien_path \
-#--envi_ckpt=$envi_path 
-#--backtranslate_interactively
+usage()
+{
+    echo "usage: run [-g | --gen | -t | --train | -p | --predict | -h]"
+}
 
 while [ "$1" != "" ]; do
     case $1 in
-        -g | --gen )           shift
-                                echo "Start to run t2t_gendata"
-                                python3 ./t2t_datagen.py \
+        -g | --gen )    echo "Start to run t2t_gendata"
+                            python3 ./t2t_datagen.py \
                                 --data_dir=./data/translate_vivi \
                                 --tmp_dir=/tmp/translate_vivi \
-                                --problem=translate_vivi
-                                $@
+                                --problem=translate_vivi \
                                 ;;
-        -t | --train )    echo "Start tp rim t2t_train"
+        -t | --train )  echo "Start to run t2t_train"
                             python3 ./t2t_trainer.py \
                                 --model='transformer' \
                                 --hparams_set=$hparams_set \
@@ -34,8 +29,15 @@ while [ "$1" != "" ]; do
                                 --output_dir=./output/translate_vivi
                                 --use_tpu=False \
                                 --worker_gpus=2 \
-                                $@
                                 ;;
+	-p | --predict )    echo "Start to run decoder"
+                            python3 ./vivi.py \
+                                --decode_hparams=$decode_hparams \
+                                --model=$model_name \
+                                --hparams_set=$hparams_set \
+                                --vivi_ckpt=$vivi_path \
+                                --vivi_interactively \
+                                ;;			
         -h | --help )           usage
                                 exit
                                 ;;
@@ -44,39 +46,5 @@ while [ "$1" != "" ]; do
     esac
     shift
 done
-
-
-if [ "$1" == "gen_data" ]; then
-    echo "Start to run t2t_gendata"
-    python3 ./t2t_datagen.py \
-	--data_dir=./data/translate_vivi \
-	--tmp_dir=/tmp/translate_vivi \
-	--problem=translate_vivi
-	$@
-else if [ "$1" == "train" ]; then
-    echo "Start tp rim t2t_train"
-    python3 ./t2t_trainer.py \
-        --model='transformer' \
-        --hparams_set=$hparams_set \
-        --hparams='learning_rate_cosine_cycle_steps=50000' \
-        --train_steps=1000 \
-        --eval_steps=10 \
-        --problem=translate_vivi \
-        --data_dir=./data/translate_vivi \
-        --output_dir=./output/translate_vivi
-        --use_tpu=False \
-        --worker_gpus=2 \
-        $@
-el
-    #python3 ./back_translate.py 
-    #--lang=vi \
-    #--decode_hparams=$decode_hparams \
-    #--model=$model_name \
-    #--hparams_set=$hparams_set \
-    #--vien_ckpt=$vien_path \
-    #--envi_ckpt=$envi_path 
-    #--backtranslate_interactively	
-    #$@
-fi
 
 
--- vivi.py
+++ vivi.py
@@ -16,29 +16,17 @@ import os
 registry = problems.registry
 
 tf.flags.DEFINE_string(
-    'vien_problem', 
-    'translate_vien_iwslt32k', 
-    'Problem name for Vietnamese to English translation.')
+    'vivi_problem', 
+    'translate_vivi', 
+    'Problem name for Vietnamese (notone) to Vietnamese translation.')
 tf.flags.DEFINE_string(
-    'envi_problem', 
-    'translate_envi_iwslt32k', 
-    'Problem name for English to Vietnamese translation.')
-tf.flags.DEFINE_string(
-    'vien_data_dir', 
-    'gs://vien-translation/data/translate_vien_iwslt32k', 
+    'vivi_data_dir', 
+    './vivi-translation/data/translate_vivi', 
     'Data directory for Vietnamese to English translation.')
 tf.flags.DEFINE_string(
-    'envi_data_dir', 
-    'gs://vien-translation/data/translate_envi_iwslt32k', 
-    'Data directory for English to Vietnamese translation.')
-tf.flags.DEFINE_string(
-    'vien_ckpt', 
-    'gs://vien-translation/checkpoints/translate_vien_iwslt32k_tiny/avg/', 
+    'vivi_ckpt', 
+    './vivi-translation/checkpoints/translate_vivi/avg/', 
     'Pretrain checkpoint directory for Vietnamese to English translation.')
-tf.flags.DEFINE_string(
-    'envi_ckpt', 
-    'gs://vien-translation/checkpoints/translate_envi_iwslt32k_tiny/avg/', 
-    'Pretrain checkpoint directory for English to Vietnamese translation.')
 tf.flags.DEFINE_string(
     'paraphrase_from_file', 
     'test_input.vi', 
@@ -47,8 +35,10 @@ tf.flags.DEFINE_string(
     'paraphrase_to_file', 
     'test_output.vi', 
     'Output text file to paraphrase.')
-
-tf.flags.DEFINE_string('lang', 'vi', 'Language of the input text file (vi or en).')
+tf.flags.DEFINE_boolean(
+    'vivi_interactively',
+    False,
+    'Whether to translate interactively.')    
 
 FLAGS = tf.flags.FLAGS
   
@@ -57,39 +47,25 @@ FLAGS = tf.flags.FLAGS
 if __name__ == '__main__':
   tf.logging.set_verbosity(tf.logging.INFO)
 
-  # Set up some decoding flags depending on the input text language.
-  if FLAGS.lang == 'vi':
-    from_data_dir, to_data_dir = FLAGS.vien_data_dir, FLAGS.envi_data_dir
-    from_problem, to_problem = FLAGS.vien_problem, FLAGS.envi_problem
-    from_ckpt, to_ckpt = FLAGS.vien_ckpt, FLAGS.envi_ckpt
-    proxy_lang = 'en'
-  elif FLAGS.lang == 'en':
-    from_data_dir, to_data_dir = FLAGS.envi_data_dir, FLAGS.vien_data_dir
-    from_problem, to_problem = FLAGS.envi_problem, FLAGS.vien_problem
-    from_ckpt, to_ckpt = FLAGS.envi_ckpt, FLAGS.vien_ckpt
-    proxy_lang = 'vi'
-  else:
-    raise ValueError('Not supported language: {}'.format(lang))
-
   # Convert directory into checkpoints
-  if tf.gfile.IsDirectory(from_ckpt):
-    from_ckpt = tf.train.latest_checkpoint(from_ckpt)
-  if tf.gfile.IsDirectory(to_ckpt):
-    to_ckpt = tf.train.latest_checkpoint(to_ckpt)
+  if tf.gfile.IsDirectory(vivi_ckpt):
+    vivi_ckpt = tf.train.latest_checkpoint(vivi_ckpt)
 
   # For back translation, we need a temporary file in the other language
   # before back-translating into the source language.
   tmp_file = os.path.join(
-      '{}.tmp.{}.txt'.format(FLAGS.paraphrase_from_file, proxy_lang)
+      '{}.tmp.vivi.txt'.format(FLAGS.paraphrase_from_file)
   )
+
+  data_dir = FLAGS.vivi_data_dir,
+  problem = FLAGS.vivi_problem,
+  ckpt = FLAGS.vivi_ckpt
+
+  if FLAGS.vivi_interactively:
+    decoding.vivi_interactively(problem, data_dir, ckpt)
 
   # Step 1: Translating from source language to the other language.
   if not tf.gfile.Exists(tmp_file):
-    decoding.t2t_decoder(from_problem, from_data_dir,
+    decoding.t2t_decoder(problem, data_dir,
                          FLAGS.paraphrase_from_file, tmp_file,
-                         from_ckpt)
-
-  # Step 2: Translating from the other language (tmp_file) to source.
-  decoding.t2t_decoder(to_problem, to_data_dir,
-                       tmp_file, FLAGS.paraphrase_to_file,
-                       to_ckpt)
+                         ckpt)
